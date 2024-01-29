f7b03f735c109939afdbbaff4a2af45b2234bec4
Nemos23 <osjacobuosjacobu@gmail.com>
2024-01-29 15:29:51 -0500

    first commit with general structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project to fin tune a LLM to several specific task. Initially it will be trained in logistics documentation',
+    author='Oscar Contreras',
+    license='',
+)
